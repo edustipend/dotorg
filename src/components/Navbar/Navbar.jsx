bd404ba26db77d1fe440001e0c5baa213754d263
EduStipend <edustipend@gmail.com>
2022-07-30 15:34:08 -0700

    feat: added navbar with brand icon and cta

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/edustipend.png';
+import './styles.css';
 
 export const Navbar = () => {
-	return <nav>
-		The nav bar
+	return <nav className='nav-wrapper'>
+		<div className='logo-wrapper'>
+			<img className='logo' src={Logo} alt='edustipend-logo' />
+			<p className='logo-text'>edustipend</p>
+		</div>
+
+		<div className='nav-links'>
+			<NavLink className='nav-link' to='/'>How it works</NavLink>
+			<NavLink className='nav-cta' to='/'>Make request</NavLink>
+		</div>
 	</nav>
 }
