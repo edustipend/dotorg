bfdd3b287cf8ef0a0dc04602a2d7dd13c79898c9
Edustipend Dev <edustipenddev@gmail.com>
2023-07-05 21:55:21 -0700

    feat: cta button, storybook setup

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,24 @@
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+export default {
+	title: 'Edustipend/Button',
+	component: Button,
+	tags: ['autodocs'],
+	argTypes: {
+		backgroundColor: { control: 'color' },
+	},
+};
+
+export const Primary = {
+	args: {
+		primary: true,
+		label: 'Request stipend',
+	},
+};
+
+export const Secondary = {
+	args: {
+		label: 'Request stipend',
+	},
+};
---
+++ src/components/Button/constants.js
@@ -0,0 +1,11 @@
+export const DEFAULT_BUTTON_LABEL = 'Add button';
+
+export const ClassName = {
+	ROOT_BUTTON: 'edustipend-button',
+	PRIMARY_BUTTON: 'edustipend-button--primary',
+	SECONDARY_BUTTON: 'edustipend-button--secondary',
+}
+
+export const TestId = {
+	DEFAULT_BUTTON_TEST_ID: 'default-button-id'
+}
