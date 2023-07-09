9450bfbdd1b5b08780cd0b83902ceab755000193
Ezeh20 <ranter1570@gmail.com>
2023-07-09 21:14:41 +0100

    fixed a conflict

---
+++ src/pages/landing/RequestNowSection/RequestNow.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button } from '../../../components/Button/Button';
+import { title } from './internals/constants';
+import Container from '../../../components/Container/Container';
+import rocket from '../../../assets/rocket emoji 3.svg';
+import styles from './RequestNow.module.css';
+import clock from '../../../assets/clock.png'
+import blobLeft from '../../../assets/blob-left.png'
+import blobRight from '../../../assets/blob-right.png'
+
+const RequestNow = () => {
+  return (
+    <main className={styles.main}>
+      <Container>
+        <div className={styles.requestContent}>
+          <h2 className={styles.title}>{title}</h2>
+          <Button label="Request Now" size='large' />
+          <img src={rocket} alt="rocket_emoji" className={styles.rocket} />
+        </div>
+      </Container>
+      <div>
+        <img src={clock} alt="clock" className={styles.clockImg} />
+      </div>
+      <img src={blobLeft} alt="blobLeft" className={`${styles.blob} ${styles.blobLeft}`} />
+      <img src={blobRight} alt="blobRight" className={`${styles.blob} ${styles.blobRight}`} />
+    </main>
+  );
+};
+
+export default RequestNow;
