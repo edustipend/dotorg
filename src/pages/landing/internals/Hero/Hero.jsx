45e3dd8c9f18d5506e0d0067663b8602f3396d43
Isiaq Ridwan Bukola <isiaqridwanbukola1999@gmail.com>
2023-07-12 10:31:29 +0100

    fix/navbar-and-hero styles

--- src/pages/landing/internals/Hero/Hero.jsx
+++ src/pages/landing/internals/Hero/Hero.jsx
@@ -1,55 +1,25 @@
+import { useEffect, useState } from 'react';
 import Container from '../../../../components/Container/container';
 import Button from '../../../../components/Button';
 import { Link } from 'react-router-dom';
 import { Hero1, Hero2, Hero3, Hero4, Svg1, Svg2, Svg3, Svg4, Svg5, ArrowDown } from '../../../../assets/index';
+import { stipends, stipendsColors } from './constants';
 
 import './styles.css';
-import { useEffect, useState } from 'react';
 
-const stipends = ['stipend', 'laptop', 'data', 'fees'];
 const Hero = () => {
   const [currentStipend, setcurrentStipend] = useState(0);
-  const [stipendClass, setStipendClass] = useState('#5801ff');
-
   const nextStipend = () => {
     setcurrentStipend(currentStipend === stipends.length - 1 ? 0 : (prev) => prev + 1);
-  };
-  const nextStipendColor = () => {
-    setStipendClass(
-      currentStipend === 0
-        ? '#5801ff'
-        : currentStipend === 1
-        ? '#FD5A33' //orange for laptop
-        : currentStipend === 2
-        ? '#08B7B7' // blue for data
-        : currentStipend === 3
-        ? '#FFB600' //yellow for fees
-        : 'black'
-    );
   };
 
   useEffect(() => {
     const timeInterval = setInterval(() => {
       nextStipend();
-      nextStipendColor();
       if (currentStipend > stipends.length - 1) {
         setcurrentStipend(0);
       }
-    }, 1000);
-    // const stipendClassName =
-    //   currentStipend === 0
-    //     ? '#5801ff'
-    //     : currentStipend === 1
-    //     ? '#FD5A33' //orange for laptop
-    //     : currentStipend === 2
-    //     ? '#08B7B7' // blue for data
-    //     : currentStipend === 3
-    //     ? '#FFB600' //yellow for fees
-    //     : 'black';
-
-    // console.log(stipendClass);
-    // console.log(stipendClassName);
-
+    }, 2000);
     return () => {
       clearInterval(timeInterval);
     };
@@ -62,7 +32,7 @@ const Hero = () => {
             In need of some{' '}
             <span
               style={{
-                color: { stipendClass }
+                color: stipendsColors[currentStipend]
               }}>
               {stipends[currentStipend]}
             </span>{' '}
---
+++ src/pages/landing/internals/Hero/constants.js
@@ -0,0 +1,2 @@
+export const stipends = ['stipend', 'laptop', 'data', 'fees'];
+export const stipendsColors = ['#5801ff', '#FD5A33', '#08B7B7', '#FFB600'];
