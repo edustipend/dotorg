8fb77075e411fcc136ec8e7517a6d39a68acdc84
Ezeh20 <ranter1570@gmail.com>
2023-07-12 18:58:31 +0100

    moved section from a diffrent branch

---
+++ src/sections/RequestNow/RequestNow.stories.js
@@ -0,0 +1,8 @@
+import RequestNow from "./RequestNow";
+
+export default {
+    title: 'Edustipend/RequestNow',
+    component: RequestNow
+}
+
+export const Request_Now = () => <RequestNow />
---
+++ src/sections/RequestNow/internals/constants.js
@@ -0,0 +1,6 @@
+export const title = 'There is no better time to start your learning journey but NOW';
+
+
+export const testConstants = {
+    componentTestId: 'componentTestId',
+}
