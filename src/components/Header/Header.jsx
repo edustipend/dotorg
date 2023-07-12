cea8609dcd69e76c09048bacdc3358fac32b1221
Ezeh20 <ranter1570@gmail.com>
2023-07-12 20:33:57 +0100

    .

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,30 +2,39 @@ import PropTypes from 'prop-types';
 import styles from './Header.module.css';
 import { TestId } from './constants';
 
+const HeaderSizes = {
+  small: 'h3',
+  medium: 'h2',
+  large: 'h1'
+};
 
-export const Header = ({ className, dataTest, color, subheader, text }) => {
+export const Header = ({ className, dataTest, size = 'large', color, subheader, text }) => {
+  const HeaderComponent = HeaderSizes[size];
   return (
-    <h1 className={[`${styles.header}`, subheader ? `${styles.subheader}` : '', `${className}`].join(' ')}
+    <HeaderComponent
+      className={[`${styles.header}`, `${styles[`header-${size}`]} ${styles[color]}`,
+      subheader ? `${styles.subheader}` : '', `${className}`].join(' ')}
       data-testid={dataTest}
-      style={{ color: `${color}` }}
     >
       {text}
-    </h1>
+    </HeaderComponent>
   );
 };
 
 Header.propTypes = {
   className: PropTypes.string,
   dataTest: PropTypes.string,
+  size: PropTypes.oneOf(['small', 'medium', 'large']),
   subheader: PropTypes.bool,
   text: PropTypes.string,
-  color: PropTypes.string,
+  color: PropTypes.string
 };
 
 Header.defaultProps = {
   className: '',
   dataTest: TestId.DEFAULT_HEADER_TEST_ID,
+  size: 'large',
   subheader: false,
   text: 'Default header text',
-  color: '#2D3740'
+  color: 'primary',
 };
