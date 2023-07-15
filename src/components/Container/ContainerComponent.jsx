a2b4cb73694d7f8c46ef4e7c41b7b8fa13941577
Edustipend Dev <edustipenddev@gmail.com>
2023-07-15 08:29:52 -0700

    chore: this should fix it

--- src/components/Container/ContainerComponent.jsx
+++ src/components/Container/ContainerComponent.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './container.module.css';
 import PropTypes from 'prop-types';
 
-const Container = ({ children }) => {
+export const Container = ({ children }) => {
   return <div className={styles.container}>{children}</div>;
 };
 
