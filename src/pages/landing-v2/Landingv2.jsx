e1c7ff3fdf6f49cc887af4cf40b426fb677d77b9
Oluwapelumi <pelumiiadetoye@gmail.com>
2023-07-17 22:56:01 +0100

    made changes to view in Landingv2

--- src/pages/landing-v2/Landingv2.jsx
+++ src/pages/landing-v2/Landingv2.jsx
@@ -1,11 +1,17 @@
 import Footer from '../../components/Footer';
 import Hero from '../../sections/Hero';
 import TestimonialsSection from '../../sections/Testimonials';
+import HowItWorksSection from '../../sections/HowItWorks';
+import BeneficiariesSection from '../../sections/Beneficiaries';
+import TakeOffBurdenSection from '../../sections/TakeOffBurden';
 
 export const LandingV2 = () => {
   return (
     <div>
       <Hero />
+      <HowItWorksSection />
+      <BeneficiariesSection />
+      <TakeOffBurdenSection />
       <TestimonialsSection />
       <Footer />
     </div>
