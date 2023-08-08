909471cc4a263b9fbdc883fb26a79228cd1d2b44
Isiaq Ridwan Bukola <isiaqridwanbukola1999@gmail.com>
2023-08-08 15:25:51 +0100

    chore/edustipend-at-one, added data to the timeline and carousel

--- src/sections/Timelines/constants.js
+++ src/sections/Timelines/constants.js
@@ -46,7 +46,8 @@ export const timelineData = [
       'The consistency of Edustipend is what amazes me the most, for me that top notch. I am joining @edustipend to celebrate their one year anniversary and also to express my gratitude to all the donors and team, and wish them all the best.',
     userProfile: '@EBakeCode',
     link: 'https://twitter.com/EBakeCode/status/1687423151280369664',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Applicant'
   },
   {
     userName: 'Habib Yusuf',
@@ -54,7 +55,8 @@ export const timelineData = [
       'I was fortunate to be a beneficiary of the June data stipend, and I must say that @edustipend has had a profound impact on my learning journey. It has been an excellent resource that significantly enhanced my learning experience."',
     userProfile: '@Cipherprog',
     link: 'https://twitter.com/Cipherprog/status/1684212370594320385',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Beneficiary'
   },
   {
     userName: 'Ifeanyichukwu Osuji',
@@ -62,7 +64,8 @@ export const timelineData = [
       'What a great platform! Edustipend have been providing resources and stipends to support learning though I am yet to benefit from them,but testimonies abound, they have touched lives and are still doing so. May God continue to bless all those who are championing this great course in helping to shape the future of so many people out there',
     userProfile: 'Ifeanyichukwu Osuji',
     link: 'https://www.linkedin.com/posts/ifeanyichukwu-osuji-84696bba_edustipendatone-activity-7093833795728007168-iIGI',
-    social: 'LinkedIn'
+    social: 'LinkedIn',
+    role: 'Applicant'
   },
   {
     userName: 'Oluwatobiloba Adebayo',
@@ -70,22 +73,25 @@ export const timelineData = [
       'From having no hope of getting my certificates and Laptop back to getting a brand new laptop worth over 200k…I want to appreciate the founder of this initiative for their willingness to invest and impact the lives of youth in Nigeria by providing resources to aid their learning and giving them a chance to dream and become the best they can be irrespective of their socio-economic background.',
     userProfile: 'Oluwatobiloba Adebayo',
     link: 'https://www.linkedin.com/pulse/dont-write-year-off-god-still-does-miracles-oluwatobiloba-adebayo',
-    social: 'LinkedIn'
+    social: 'LinkedIn',
+    role: 'Beneficiary'
   },
   {
-    userName: 'Priscilla Andideh',
+    userName: 'Priscilla Adindeh',
     content:
       "You've impacted the lives of so many and I'm so grateful to be a part of those that have benefited from the monthly stipend. Thanks for believing in us, our passion and dreams. Many more years to celebrate. Congratulations 🎉",
     userProfile: '@PAndideh',
     link: 'https://twitter.com/PAdindeh/status/1684293641823477766?',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Beneficiary'
   },
   {
     userName: 'David Kelechi',
     content: 'These guys are really awesome doing what they do👏👏👏',
     userProfile: '@david1kelechi',
     link: 'https://twitter.com/david1kelechi/status/1639568311959601152',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Public'
   },
   {
     userName: 'Blessing Agabus',
@@ -93,7 +99,8 @@ export const timelineData = [
       "@edustipend community is doing a lot better to the heart and career-building of its beneficiaries. I introduced my girlfriend last month, she got a voucher aid. Mine is a laptop, I'm never giving up.",
     userProfile: '@FineAgabus',
     link: 'https://twitter.com/FineAgabus/status/1622955318362734593',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Public'
   },
   {
     userName: 'Obika Viola',
@@ -101,7 +108,8 @@ export const timelineData = [
       "I'm really really grateful to @edustipend for this great opportunity they've given me🥳🎊🥳. I'm really loving it and I'm looking forward to improving on my learning with this new learning device and of course take a chance on others 🥰.",
     userProfile: '@Obika_viola',
     link: 'https://twitter.com/obika_viola/status/1609493836526501890',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Beneficiary'
   },
   {
     userName: 'Philip Ishola',
@@ -109,14 +117,33 @@ export const timelineData = [
       'Angels still exists in humans. @edustipend is an angelic organisation. I want to appreciate the @edustipend team for believing in my dream and giving a proportional support to my career. Thank you, I will definitely pay it forward.',
     userProfile: '@Prereloluwa',
     link: 'https://twitter.com/prereloluwa/status/1601244577520046082',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Beneficiary'
   },
   {
     userName: 'Chidiebere D. Ekwedike',
     content: 'Perfect definition of we rise by lifting others. @edustipend setting the pace by helping the needs of techies. 👏🏽',
     userProfile: '@DamianEkwedike',
     link: 'https://twitter.com/DamianEkwedike/status/1571145363381448704',
-    social: 'Twitter'
+    social: 'Twitter',
+    role: 'Public'
+  },
+  {
+    userName: 'Deborah Akujobi',
+    content:
+      "The growth I have experienced, since I became an ambassador for @edustipend it's been three months of consistent weekly check in and activities. Hosted a webinar and seminar, spoke on a twitter space for the first time. The confidence this experience has given me",
+    userProfile: '@Debra_Brown',
+    link: 'https://twitter.com/Debra_Brown/status/1656338781274796032',
+    social: 'Twitter',
+    role: 'Ambassador'
+  },
+  {
+    userName: 'Ubon Udonkang',
+    content: 'With @edustipend, I went from an applicant to becoming a beneficiary, then an ambassador then securing my first offer as a UX designer',
+    userProfile: '@ubondesigner',
+    link: 'https://twitter.com/ubondesigner/status/1663839403188400128',
+    social: 'Twitter',
+    role: 'Ambassador'
   }
 ];
 
