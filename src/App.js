ca6234466aad9b32fc90da9ee31a244aee44abfa
Ezeh20 <ranter1570@gmail.com>
2023-08-08 13:27:36 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import initFirebaseApp from './firebaseConfig';
 import Navbar from './components/Navbar';
 import './App.css';
 import Footer from './components/Footer';
-import NotifyModal from './components/NotifyModal';
+import NotifyModal from './sections/NotifyMe/internals/NotifyModal';
 import NotifyMe from './sections/NotifyMe';
 initFirebaseApp();
 
--- src/sections/NotifyMe/internals/NotifyModal/NotifyModal.jsx
+++ src/sections/NotifyMe/internals/NotifyModal/NotifyModal.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ModalContext } from '../../context/ModalContext';
+import { ModalContext } from '../../../../context/ModalContext';
 import { useContext } from 'react';
 
 export const NotifyModal = ({ children, className }) => {
