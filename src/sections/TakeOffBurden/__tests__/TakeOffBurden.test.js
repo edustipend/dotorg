3a4ce49f99ea26d1b56805af4eae2f22362080d8
Ezeh20 <ranter1570@gmail.com>
2023-08-08 02:36:44 +0100

    fix: fixed failing tests

--- src/sections/TakeOffBurden/__tests__/TakeOffBurden.test.js
+++ src/sections/TakeOffBurden/__tests__/TakeOffBurden.test.js
@@ -2,10 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import TakeOffBurden from '../TakeOffBurden';
 import { ButtonLabelCopy, TestId, TextCopy } from '../constants';
+import { ModalContextProvider } from '../../../context/ModalContext';
 
 describe('TakeOffBurden', () => {
   it('renders the component correctly', () => {
-    render(<TakeOffBurden />);
+    render(
+      <ModalContextProvider>
+        <TakeOffBurden />
+      </ModalContextProvider>
+    );
     expect(screen.getByTestId(TestId.TAKEOFFBURDEN_CONTAINER_TEST_ID)).toBeInTheDocument();
     expect(screen.getByTestId(TestId.TAKEOFFBURDEN_CARD_TEST_ID)).toBeInTheDocument();
     expect(screen.getByTestId(TestId.TAKEOFFBURDEN_TRANSPARENT_CARD_TEST_ID)).toBeInTheDocument();
