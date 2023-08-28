ac0130c44b91d648c5008c2c90487f7a7db4f985
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-08-28 15:52:26 +0100

    feature/welcome, refactored the sidebar rendering

--- src/pages/learner-dashboard/LearnerDashboard.jsx
+++ src/pages/learner-dashboard/LearnerDashboard.jsx
@@ -1,4 +1,3 @@
-// import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import styles from './LearnerDashboard.module.css';
 import SideBar from '../../components/SideBar';
@@ -7,19 +6,31 @@ import { Close, Menu } from '../../assets';
 import { useContext } from 'react';
 import { useEffect } from 'react';
 import InstallPrompt from '../../components/InstallPrompt';
+import { useState } from 'react';
 
 export const LearnerDashboard = () => {
   const { showSidebar, setShowSidebar } = useContext(SidebarCtx);
+  const [desktopScreen, setDesktopScreen] = useState(document.body.clientWidth);
 
   useEffect(() => {
-    const desktopScreen = document.body.clientWidth >= 1024;
-    desktopScreen && setShowSidebar(true);
-  });
+    const handleResize = () => {
+      const newScreenWidth = window.innerWidth;
+      setDesktopScreen(newScreenWidth);
+      setShowSidebar(newScreenWidth >= 1024);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [setShowSidebar]);
 
   return (
     <div className={styles.main}>
       <div>
-        <SideBar />
+        {desktopScreen && <SideBar />}
         <div className={styles.menu} onClick={() => setShowSidebar(!showSidebar)}>
           <img src={showSidebar ? Close : Menu} alt="menu" />
         </div>
