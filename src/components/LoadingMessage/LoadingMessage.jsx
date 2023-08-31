525af99abc4f3d03d7345785af8062151ee309ae
Ezeh20 <ranter1570@gmail.com>
2023-08-31 18:08:41 +0100

    staging branch

--- src/components/LoadingMessage/LoadingMessage.jsx
+++ src/components/LoadingMessage/LoadingMessage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Loader from '../Loader';
 import styles from './LoadingMessage.module.css';
+
 import { messages } from './internals/constants';
 import randomMessage from '../../utils/GenerateRandomMessage';
 
