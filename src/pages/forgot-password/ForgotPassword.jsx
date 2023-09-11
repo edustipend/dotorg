4e3b7760029788107b25a5b374f73bf3646e1889
Ezeh20 <ranter1570@gmail.com>
2023-09-11 08:24:20 +0100

    ready to push

--- src/pages/forgot-password/ForgotPassword.jsx
+++ src/pages/forgot-password/ForgotPassword.jsx
@@ -15,110 +15,110 @@ import styles from './ForgotPassword.module.css';
 const { HEADER, TEXT, FOOT_NOTE } = constants;
 
 export const ForgotPassword = () => {
-    const { setIsActive } = useContext(ModalContext);
-    const [state, dispatch] = useReducer(ForgotReducer, INITIAL_STATE)
-    const { LOADING, DISABLED, ERROR, SUCCESS, EMAIL, ON_SUCCESS, ON_ERROR } = Types
-    const { email, disabled, feedBack, loading, success, error } = state
-    const isValid = checkEmail(email);
+  const { setIsActive } = useContext(ModalContext);
+  const [state, dispatch] = useReducer(ForgotReducer, INITIAL_STATE);
+  const { LOADING, DISABLED, ERROR, SUCCESS, EMAIL, ON_SUCCESS, ON_ERROR } = Types;
+  const { email, disabled, feedBack, loading, success, error } = state;
+  const isValid = checkEmail(email);
 
-    useEffect(() => {
-        if (isValid) {
-            dispatch({ type: DISABLED, payload: false })
-        }
-    }, [DISABLED, dispatch, isValid]);
+  useEffect(() => {
+    if (isValid) {
+      dispatch({ type: DISABLED, payload: false });
+    }
+  }, [DISABLED, dispatch, isValid]);
 
-    const sendMail = async () => {
-        dispatch({ type: LOADING, payload: true })
-        const { success, message } = await postData('reset-password', { email, name: 'default' });
-        if (success) {
-            dispatch({ type: ON_SUCCESS, payload: message })
-            setIsActive((prev) => !prev);
-        } else if (!success) {
-            dispatch({ type: ON_ERROR, payload: message })
-            setIsActive((prev) => !prev);
-        }
-    };
-
+  const sendMail = async () => {
+    dispatch({ type: LOADING, payload: true });
+    const { success, message } = await postData('reset-password', { email, name: 'default' });
     if (success) {
-        return (
-            <section className={styles.main}>
-                <div className={styles.contentContainer}>
-                    <Modal>
-                        <div className={styles.Modal}>
-                            <img src={Valid} alt="valid email" className={styles.img} />
-                            <p className={styles.feedBack}>{feedBack}</p>
-                            <Button
-                                effectAlt
-                                label={'close'}
-                                type={'secondary'}
-                                onClick={() => {
-                                    dispatch({ type: SUCCESS, payload: false });
-                                    setIsActive((prev) => !prev);
-                                }}
-                            />
-                        </div>
-                    </Modal>
-                </div>
-            </section>
-        );
+      dispatch({ type: ON_SUCCESS, payload: message });
+      setIsActive((prev) => !prev);
+    } else if (!success) {
+      dispatch({ type: ON_ERROR, payload: message });
+      setIsActive((prev) => !prev);
     }
+  };
 
-    if (error) {
-        return (
-            <section className={styles.main}>
-                <div className={styles.contentContainer}>
-                    <Modal>
-                        <div className={styles.Modal}>
-                            <img src={Tears} alt="invalid email" className={styles.imgAlt} />
-                            <p className={styles.feedBack}>{feedBack}</p>
-                            <Button
-                                effectAlt
-                                label={'close'}
-                                type={'secondary'}
-                                onClick={() => {
-                                    dispatch({ type: ERROR, payload: false });
-                                    setIsActive((prev) => !prev);
-                                }}
-                            />
-                        </div>
-                    </Modal>
-                </div>
-            </section>
-        );
-    }
-
+  if (success) {
     return (
-        <section className={styles.main}>
-            <div className={styles.contentContainer}>
-                <div className={styles.content}>
-                    <img src={Tears} alt="wrong password" className={styles.img} />
-                    <Header className={styles.header}>{HEADER}</Header>
-                    <Text className={styles.text} content={TEXT} />
-                    <Input
-                        value={email}
-                        onChange={(e) => dispatch({ type: EMAIL, payload: e.target.value })}
-                        label={'Email Address'}
-                        className={styles.input}
-                        mainClass={styles.mainClass}
-                    />
-                    <div className={styles.btnContainer}>
-                        <Button
-                            value
-                            effectAlt
-                            isLoading={loading}
-                            loaderSize={'small'}
-                            loaderVariant={'neutral'}
-                            label={'Send instructions'}
-                            type={'secondary'}
-                            size={'medium'}
-                            disabled={disabled}
-                            onClick={sendMail}
-                            className={styles.btn}
-                        />
-                    </div>
-                    <Text className={styles.footNote} content={FOOT_NOTE} />
-                </div>
+      <section className={styles.main}>
+        <div className={styles.contentContainer}>
+          <Modal>
+            <div className={styles.Modal}>
+              <img src={Valid} alt="valid email" className={styles.img} />
+              <p className={styles.feedBack}>{feedBack}</p>
+              <Button
+                effectAlt
+                label={'close'}
+                type={'secondary'}
+                onClick={() => {
+                  dispatch({ type: SUCCESS, payload: false });
+                  setIsActive((prev) => !prev);
+                }}
+              />
             </div>
-        </section>
+          </Modal>
+        </div>
+      </section>
     );
+  }
+
+  if (error) {
+    return (
+      <section className={styles.main}>
+        <div className={styles.contentContainer}>
+          <Modal>
+            <div className={styles.Modal}>
+              <img src={Tears} alt="invalid email" className={styles.imgAlt} />
+              <p className={styles.feedBack}>{feedBack}</p>
+              <Button
+                effectAlt
+                label={'close'}
+                type={'secondary'}
+                onClick={() => {
+                  dispatch({ type: ERROR, payload: false });
+                  setIsActive((prev) => !prev);
+                }}
+              />
+            </div>
+          </Modal>
+        </div>
+      </section>
+    );
+  }
+
+  return (
+    <section className={styles.main}>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <img src={Tears} alt="wrong password" className={styles.img} />
+          <Header className={styles.header}>{HEADER}</Header>
+          <Text className={styles.text} content={TEXT} />
+          <Input
+            value={email}
+            onChange={(e) => dispatch({ type: EMAIL, payload: e.target.value })}
+            label={'Email Address'}
+            className={styles.input}
+            mainClass={styles.mainClass}
+          />
+          <div className={styles.btnContainer}>
+            <Button
+              value
+              effectAlt
+              isLoading={loading}
+              loaderSize={'small'}
+              loaderVariant={'neutral'}
+              label={'Send instructions'}
+              type={'secondary'}
+              size={'medium'}
+              disabled={disabled}
+              onClick={sendMail}
+              className={styles.btn}
+            />
+          </div>
+          <Text className={styles.footNote} content={FOOT_NOTE} />
+        </div>
+      </div>
+    </section>
+  );
 };
