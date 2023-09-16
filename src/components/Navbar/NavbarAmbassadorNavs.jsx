c59915e60b5bd777c5ee5211fee0afd016bc247c
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-09-16 18:09:19 +0100

    feature/ambassador-cta

--- src/components/Navbar/NavbarAmbassadorNavs.jsx
+++ src/components/Navbar/NavbarAmbassadorNavs.jsx
@@ -11,25 +11,18 @@ const NavbarAmbassadorNavs = ({ showMenu, closeMenu }) => {
       <nav className="navbarNavs" data-testid={NAVBAR_LINKS_ID}>
         {NAVBAR_AMBASSADOR_LINKS.map((link) => (
           <NavHashLink key={link.label} to={{ pathname: link.path, hash: link.hash }}>
-            {link.label === 'Apply now' ? <Button label={link.label} type={'secondary'} /> : link.label}
+            {link.label === 'Apply Now' ? <Button label={link.label} type={'secondary'} /> : link.label}
           </NavHashLink>
         ))}
       </nav>
 
       {showMenu ? (
         <nav className="mobile-nav">
-          {NAVBAR_AMBASSADOR_LINKS.map((link) =>
-            link.label !== 'Apply Now' ? (
-              <NavHashLink key={link.label} to={{ pathname: link.path, hash: link.hash }} onClick={() => closeMenu(!showMenu)}>
-                {link.label}
-              </NavHashLink>
-            ) : null
-          )}
-          <NavHashLink to={{ pathname: '/ambassador-program', hash: '#apply-now' }} onClick={() => closeMenu(!showMenu)}>
-            <div className="mobile-nav-btn">
-              <Button label={'Apply now'} type={'secondary'} />
-            </div>
-          </NavHashLink>
+          {NAVBAR_AMBASSADOR_LINKS.map((link) => (
+            <NavHashLink key={link.label} to={{ pathname: link.path, hash: link.hash }} onClick={() => closeMenu(!showMenu)}>
+              {link.label}
+            </NavHashLink>
+          ))}
         </nav>
       ) : (
         <nav className="mobile-nav out" />
