7055f6a821bbd7d9293e85640bc3ef2702c3f3e5
Ezeh20 <ranter1570@gmail.com>
2023-09-20 10:52:24 +0100

    extracted the email and code from the URL

--- src/pages/reset-password/ResetPassword.jsx
+++ src/pages/reset-password/ResetPassword.jsx
@@ -5,13 +5,16 @@ import styles from './ResetPassword.module.css';
 import Header from '../../components/Header';
 import { constants } from './constants';
 
-const { HEADER, EMAIL, PASSWORD, CODE, CONFIRM_PASSWORD } = constants;
+const { HEADER, EMAIL, PASSWORD, CODE, CONFIRM_PASSWORD, PASSWORD_MIN_LENGTH_ERR, PASSWORD_MISMATCH } = constants;
 
 export const ResetPassword = () => {
     const [email, setEmail] = useState('');
     const [code, setCode] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordErr, setPasswordErr] = useState(false);
+    const [passwordMisMatch, setPasswordMisMatch] = useState(false);
+
 
     useEffect(() => {
         if (window.location.search) {
@@ -22,6 +25,20 @@ export const ResetPassword = () => {
             setCode(Code);
         }
     }, []);
+
+    const onSubmit = async () => {
+        const min_length = 8
+        if (password !== confirmPassword) {
+            setPasswordMisMatch(true)
+            return;
+        } else if (password.length < min_length) {
+            setPasswordErr(true)
+            return;
+        } else {
+            setPasswordMisMatch(false)
+            setPasswordErr(false)
+        }
+    }
 
     return (
         <section className={styles.reset}>
@@ -34,22 +51,30 @@ export const ResetPassword = () => {
                             <Input value={code} label={CODE} placeholder={CODE} readOnly />
                         </div>
                         <div className={styles.field}>
-                            <Input
-                                value={password}
-                                label={PASSWORD}
-                                placeholder={PASSWORD}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                            <Input
-                                value={confirmPassword}
-                                label={CONFIRM_PASSWORD}
-                                placeholder={CONFIRM_PASSWORD}
-                                onChange={(e) => setConfirmPassword(e.target.value)}
-                            />
+                            <div className={styles.single}>
+                                <Input
+                                    type={'password'}
+                                    value={password}
+                                    label={PASSWORD}
+                                    placeholder={PASSWORD}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                {passwordErr ? <small className={styles.small}>{PASSWORD_MIN_LENGTH_ERR}</small> : undefined}
+                            </div>
+                            <div className={styles.single}>
+                                <Input
+                                    type={'password'}
+                                    value={confirmPassword}
+                                    label={CONFIRM_PASSWORD}
+                                    placeholder={CONFIRM_PASSWORD}
+                                    onChange={(e) => setConfirmPassword(e.target.value)}
+                                />
+                                {passwordMisMatch ? <small className={styles.small}>{PASSWORD_MISMATCH}</small> : undefined}
+                            </div>
                         </div>
                     </div>
                     <div className={styles.btnContainer}>
-                        <Button type={'secondary'} size={'large'} label={'Submit'} className={styles.btn} />
+                        <Button type={'secondary'} size={'large'} label={'Submit'} onClick={onSubmit} className={styles.btn} />
                     </div>
                 </div>
             </div>
