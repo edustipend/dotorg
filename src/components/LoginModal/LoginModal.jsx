15242a30dfdcd148b64c65f053a90d5ab9ec1ff3
Ezeh20 <ranter1570@gmail.com>
2023-11-11 16:26:15 +0100

    completed the login modal

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -1,7 +1,6 @@
 import { useCallback, useEffect, useState } from 'react';
 import Button from '../Button';
 import styles from './LoginModal.module.css';
-// import Modal from '../Modal';
 import { ModalContext } from '../../context/ModalContext';
 import { useContext } from 'react';
 import { CloseAlt, Hand } from '../../assets';
@@ -30,8 +29,8 @@ const disabledButtons = {
 };
 
 const mockedExpectations = {
-    existsInOldDatabase: true,
-    hasResetPassword: false
+    existsInOldDatabase: false,
+    hasResetPassword: true
 };
 const { existsInOldDatabase, hasResetPassword } = mockedExpectations;
 export const LoginModal = () => {
@@ -63,7 +62,7 @@ export const LoginModal = () => {
 
     //mocked function to manage login proccess
     const handleContinue = () => {
-        if (hasResetPassword) {
+        if (hasResetPassword && existsInOldDatabase) {
             setLoading(true);
             setTimeout(() => {
                 setLoading(false);
@@ -82,6 +81,7 @@ export const LoginModal = () => {
             setLoading(true);
             setTimeout(() => {
                 setLoading(false);
+                setDisabled({ ...disabled, continueBtn: true });
                 setFeedback({ ...feedback, newUser: constant.NEW_USER });
                 setSecondaryButton(true);
             }, 2000);
@@ -100,70 +100,74 @@ export const LoginModal = () => {
     }, [handleLoginModal, handleNotifyModal, nav, applicationClosedState]);
 
     return (
-        <div className={`modal_modal modal_content`}>
+        <div className="modal_modal modal_content">
             <div className="animate_modal_modal">
-                <section className={styles.ModalContainer}>
-                    <div className={styles.close}>
-                        <img src={CloseAlt} alt="close" onClick={() => handleLoginModal((prev) => !prev)} />
-                    </div>
-                    <div className={styles.header}>
-                        <img src={Hand} alt="hand" className={styles.hand} />
-                        <div className={styles.headerText}>
-                            <p className={styles.head}>{constant.HEADER}</p>
-                            <p className={styles.subHeader}>{constant.SUBHEADER(subText)}</p>
+                <div className={styles.loginContent}>
+                    <section className={styles.ModalContainer}>
+                        <div className={styles.close}>
+                            <img src={CloseAlt} alt="close" onClick={() => handleLoginModal((prev) => !prev)} />
                         </div>
-                    </div>
-                    {!shouldContinue ? (
-                        <>
-                            <div>
-                                <Input
-                                    placeholder="email"
-                                    label={constant.EMAIL}
-                                    value={email}
-                                    onChange={(e) => setDetails((prev) => ({ ...prev, email: e.target.value }))}
-                                />
-                                {emailSent && <small className={styles.feedbackSuccess}>{emailSent}</small>}
-                                {newUser && <small className={styles.feedbackError}>{newUser}</small>}
+                        <div className={styles.header}>
+                            <img src={Hand} alt="hand" className={styles.hand} />
+                            <div className={styles.headerText}>
+                                <p className={styles.head}>{constant.HEADER}</p>
+                                <p className={styles.subHeader}>{constant.SUBHEADER(subText)}</p>
                             </div>
-                            <div className={styles.btnContainer}>
-                                {secondaryButton ? (
-                                    <Button label={applicationClosedState ? 'Notify me' : 'Register'} type="secondary" onClick={handleSecondaryButton} />
-                                ) : null}
-                                <Button
-                                    disabled={continueBtn}
-                                    label={constant.CONTINUE}
-                                    type="secondary"
-                                    onClick={handleContinue}
-                                    isLoading={loading}
-                                    loaderSize="small"
-                                    loaderVariant="neutral"
-                                />
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <div>
-                                <Input
-                                    type="password"
-                                    placeholder="password"
-                                    label={constant.PASSWORD}
-                                    value={password}
-                                    onChange={(e) => setDetails((prev) => ({ ...prev, password: e.target.value }))}
-                                />
-                            </div>
-                            <div className={styles.btnContainer}>
-                                <Button
-                                    disabled={submitBtn}
-                                    label={constant.SUBMIT}
-                                    type="secondary"
-                                    isLoading={loading}
-                                    loaderSize="small"
-                                    loaderVariant="neutral"
-                                />
-                            </div>
-                        </>
-                    )}
-                </section>
+                        </div>
+                        {!shouldContinue ? (
+                            <>
+                                <div>
+                                    <Input
+                                        placeholder="email"
+                                        label={constant.EMAIL}
+                                        value={email}
+                                        onChange={(e) => setDetails((prev) => ({ ...prev, email: e.target.value }))}
+                                    />
+                                    {emailSent && <small className={styles.feedbackSuccess}>{emailSent}</small>}
+                                    {newUser && <small className={styles.feedbackError}>{newUser}</small>}
+                                </div>
+                                <div className={styles.btnContainer}>
+                                    {secondaryButton ? (
+                                        <Button label={applicationClosedState ? 'Notify me' : 'Register'} type="secondary" onClick={handleSecondaryButton} className={styles.button} />
+                                    ) : null}
+                                    <Button
+                                        disabled={continueBtn}
+                                        label={constant.CONTINUE}
+                                        type="secondary"
+                                        onClick={handleContinue}
+                                        isLoading={loading}
+                                        loaderSize="small"
+                                        loaderVariant="neutral"
+                                        className={styles.button}
+                                    />
+                                </div>
+                            </>
+                        ) : (
+                            <>
+                                <div>
+                                    <Input
+                                        type="password"
+                                        placeholder="password"
+                                        label={constant.PASSWORD}
+                                        value={password}
+                                        onChange={(e) => setDetails((prev) => ({ ...prev, password: e.target.value }))}
+                                    />
+                                </div>
+                                <div className={styles.btnContainer}>
+                                    <Button
+                                        disabled={submitBtn}
+                                        label={constant.SUBMIT}
+                                        type="secondary"
+                                        isLoading={loading}
+                                        loaderSize="small"
+                                        loaderVariant="neutral"
+                                        className={styles.button}
+                                    />
+                                </div>
+                            </>
+                        )}
+                    </section>
+                </div>
             </div>
             <NotifyModal>
                 <NotifyMe />
