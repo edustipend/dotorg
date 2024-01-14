3eb42c1b77865d9d0c10e8a12eaf9dca3f5c2f71
Edustipend Dev <edustipenddev@gmail.com>
2024-01-14 00:46:25 -0800

    fix: added logic to close user exist modal on route to login page

--- src/sections/EmailExist/EmailExist.jsx
+++ src/sections/EmailExist/EmailExist.jsx
@@ -8,12 +8,16 @@ import { CLOSEMODAL_ALT_TEXT, TestId } from './constants';
 export const EmailExist = () => {
   const { setIsActive } = useContext(ModalContext);
 
+  const handleNavigate = () => {
+    setIsActive(false);
+  };
+
   return (
     <main className={styles.emailexist} data-testid={TestId.EMAIL_TEST_ID}>
       <div className={styles.closeContainer} data-testid={TestId.CLOSEMODAL_TEST_ID} onClick={() => setIsActive((prev) => !prev)}>
         <img src={closeIcon} alt={CLOSEMODAL_ALT_TEXT} className={styles.closeModal} />
       </div>
-      <EmailExistForm />
+      <EmailExistForm handleNavigate={handleNavigate} />
     </main>
   );
 };
