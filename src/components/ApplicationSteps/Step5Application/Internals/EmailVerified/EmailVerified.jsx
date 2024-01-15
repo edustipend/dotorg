56ea1c6192e97b6f1475635f1c0f56744451a31f
Ezeh Chijioke <chijioke@clivealliance.com>
2024-01-15 08:40:47 +0100

    staging

--- src/components/ApplicationSteps/Step5Application/Internals/EmailVerified/EmailVerified.jsx
+++ src/components/ApplicationSteps/Step5Application/Internals/EmailVerified/EmailVerified.jsx
@@ -29,6 +29,7 @@ export const EmailVerified = () => {
   const { isAuthenticated } = useSelector((state) => state.user);
   const [searchParams] = useSearchParams();
   const emailToken = searchParams.get('jwt');
+  
 
   const verifyEmail = useCallback(async () => {
     dispatch(emailVerification(false));
