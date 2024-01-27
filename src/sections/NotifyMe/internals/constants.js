a674c4b982b64af9b868cc22e48db9615bfd2145
Edustipend Dev <edustipenddev@gmail.com>
2024-01-27 07:11:17 -0800

    chore: merge jan 2024 release to develop, add notify feature changes

--- src/sections/NotifyMe/internals/constants.js
+++ src/sections/NotifyMe/internals/constants.js
@@ -4,6 +4,7 @@ export const formConstants = {
   SUBTITLE: 'Application window is currently closed. Please fill in the details below, so we can inform you when the next opening begins.',
   FULLNAME: 'Your name',
   EMAIL: 'Your Email address',
-  SUCCESS: 'Success',
-  WAITLIST_SUCCESS: 'Wohoo! You have signed up successfully to receive an email notification when the application window opens.'
+  SUCCESS: 'Success!',
+  WAITLIST_SUCCESS: 'Congratulations, you have signed up successfully to receive an email notification when the next application window opens.',
+  INFO_SUCCESS: 'The application window opens on the 1st of next month.'
 };
