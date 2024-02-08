33605dd8d4ace4bbb20803cbe6a81b590bc8e9d9
Adeleke Moyinoluwa Solomon <pelumiiadetoye@gmail.com>
2024-02-08 15:19:11 +0100

    Changing image of beneficiaries and quotes on the Edustipend Impacts

--- src/sections/SupportALearner/HeroSection/HeroSection.jsx
+++ src/sections/SupportALearner/HeroSection/HeroSection.jsx
@@ -8,6 +8,7 @@ import Button from '../../../components/Button';
 import quoteU from '../../../assets/quoteup.png';
 import quoteD from '../../../assets/quotedown.png';
 import { NavHashLink } from 'react-router-hash-link';
+// import Container from '../../../components/Container';
 
 export const HeroSection = () => {
   const quotesArray = [
@@ -52,50 +53,52 @@ export const HeroSection = () => {
   const currentQuote = quotesArray[currentQuoteIndex];
 
   return (
-    <div className={styles.container}>
-      <div className={styles.quotes} key={animationKey} style={{ animation: `${styles.fadeIn} 0.5s ease-in-out` }}>
-        <div className={styles.benimagediv}>
-          <img src={currentQuote.image} alt="beneficiary-image-and-quote" className={styles.benimage} />
-        </div>
-        <div className={styles.quotecont} style={{ animation: `${styles.fade} 0.5s ease-in` }}>
-          <div className={styles.quot}>
-            <img src={quoteU} alt="quote-icon" className="quoteU" style={{ alignSelf: 'flex-start' }} />
-            <Text content={currentQuote.content} className={styles.quote} />
-            <img src={quoteD} alt="quote-icon" className="quoteD" style={{ alignSelf: 'flex-end' }} />
+    // <Container>
+      <div className={styles.container}>
+        <div className={styles.quotes} key={animationKey} style={{ animation: `${styles.fadeIn} 0.5s ease-in-out` }}>
+          <div className={styles.benimagediv}>
+            <img src={currentQuote.image} alt="beneficiary-image-and-quote" className={styles.benimage} />
           </div>
-          <div className={styles.ben}>
-            <Text content={currentQuote.author} className={styles.benName} />
+          <div className={styles.quotecont} style={{ animation: `${styles.fade} 0.5s ease-in` }}>
+            <div className={styles.quot}>
+              <img src={quoteU} alt="quote-icon" className="quoteU" style={{ alignSelf: 'flex-start' }} />
+              <Text content={currentQuote.content} className={styles.quote} />
+              <img src={quoteD} alt="quote-icon" className="quoteD" style={{ alignSelf: 'flex-end' }} />
+            </div>
+            <div className={styles.ben}>
+              <Text content={currentQuote.author} className={styles.benName} />
+            </div>
           </div>
         </div>
-      </div>
-      <div className={styles.cta}>
-        <Header size={'small'} className={styles.header}>
-          Great futures are built with a little charity{' '}
-        </Header>
-        <Header size={'large'} className={styles.headerweb}>
-          Great futures are built with a little charity{' '}
-        </Header>
-        <Text
-          content={'Bridge the Educational Gap by providing resources to Transform Lives and Shape a Brighter Future for Learners Worldwide.'}
-          className={styles.text}
-        />
-        <div className={styles.ctabtns}>
-          <NavHashLink
-            to={{
-              pathname: '/support-a-learner/donate'
-            }}>
-            <Button label={'Support a Learner'} size={'small'} type={'secondary'} />
-          </NavHashLink>
+        <div className={styles.cta}>
+          <Header size={'small'} className={styles.header}>
+            Great futures are built with a little charity{' '}
+          </Header>
+          <Header size={'large'} className={styles.headerweb}>
+            Great futures are built with a little charity{' '}
+          </Header>
+          <Text
+            content={'Bridge the Educational Gap by providing resources to Transform Lives and Shape a Brighter Future for Learners Worldwide.'}
+            className={styles.text}
+          />
+          <div className={styles.ctabtns}>
+            <NavHashLink
+              to={{
+                pathname: '/support-a-learner/donate'
+              }}>
+              <Button label={'Support a Learner'} size={'small'} type={'secondary'} />
+            </NavHashLink>
 
-          <NavHashLink
-            to={{
-              pathname: '/support-a-learner',
-              hash: '#how-much-can-I-donate'
-            }}>
-            <Button label={'How much can I donate?'} size={'small'} type={'plain'} />
-          </NavHashLink>
+            <NavHashLink
+              to={{
+                pathname: '/support-a-learner',
+                hash: '#how-much-can-I-donate'
+              }}>
+              <Button label={'How much can I donate?'} size={'small'} type={'plain'} />
+            </NavHashLink>
+          </div>
         </div>
       </div>
-    </div>
+    // </Container>
   );
 };
