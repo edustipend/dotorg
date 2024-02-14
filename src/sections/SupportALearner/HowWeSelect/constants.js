40fe63f423c42c783b53b9b0596f41bf1b0ac186
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2024-02-14 12:25:36 +0100

    conflicts resolved

--- src/sections/SupportALearner/HowWeSelect/constants.js
+++ src/sections/SupportALearner/HowWeSelect/constants.js
@@ -5,31 +5,31 @@ export const how = [
     label: 'Genuine Need',
     id: '01',
     description:
-      'Our vision is to support folks who really need a push with their lerning goals. As part of the application, you should show how you really need the stipend and how getting the stipend can help you chieve your learning goals '
+      'Our vision is to support folks who really need a push with their lerning goals. As part of the application, you should show how you really need the stipend and how getting the stipend can help you achieve your learning goals.'
   },
   {
     label: 'PROGRESS SO FAR',
     id: '02',
     description:
-      'Everyone needs a laptop or data, we would like to see what they have achieved so far without the stipend . It could be a course or community you joined. We know it can be hard but the more you can demonstrate progress despite the constraints, the better your chances'
+      'Everyone needs a laptop or data, we would like to see what they have achieved so far without the stipend. It could be a course you started or a community you joined. We know it can be hard but the more you can demonstrate progress despite the constraints, the better your chances.'
   },
   {
     label: 'DEMOGRAPHY',
     id: '03',
     description:
-      'Everyone deserves a fair chance regardless of age, gender, state of origin, and we try to follow that, as much as possible. This means we will try to ensure that the stipends are evenly distributed across age, gender, and other demography'
+      'Everyone deserves a fair chance regardless of age, gender, state of origin, and we try to follow that, as much as possible. This means we will try to ensure that the stipends are evenly distributed across age, gender, and other demography.'
   },
   {
     label: 'ATTENTION TO DETAIL',
     id: '04',
     description:
-      'We do not ask for an entire epistle when asbpart of your application but we reall appreciate thoughtful responses. So answer all questions as descriptive yet concisely  as possible . Let your personality show in your application'
+      'We do not ask for an entire epistle when completing your application but we really appreciate thoughtful responses. So answer all questions as descriptively yet concisely as possible. Let your personality show in your application.'
   },
   {
     label: 'FUTURE IMPACT',
     id: '05',
     description:
-      'Core to our vision here at Edustipend is the idea of paying it forward. As part of the application, state how you plan to pay it forward if you get selected - how you plan to impact others as a result of this stipend because we believe the impact compounds'
+      'Core to our vision here at Edustipend is the idea of paying it forward. As part of the application, state how you plan to pay it forward if you get selected - how you plan to impact others as a result of this stipend because we believe the impact compounds.'
   }
 ];
 
