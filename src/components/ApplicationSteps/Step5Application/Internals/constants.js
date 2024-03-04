ad440647eade15c7ef6348e4477267db3364aa60
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2024-03-04 14:33:37 +0100

    march release date

--- src/components/ApplicationSteps/Step5Application/Internals/constants.js
+++ src/components/ApplicationSteps/Step5Application/Internals/constants.js
@@ -2,7 +2,7 @@ export const constants = {
   HEADER: 'Ready to send it off? 🚀',
   PARA1: `You've reached the final step of your application journey! Now, let's submit your hard work and leave the rest to us.`,
   PARA2: `Once you've hit that submit button, our team will carefully review your application. If you're selected for the category you applied to, we'll reach out to you using the email you provided.
-    Beneficiaries will be contacted at most three weeks from Friday 26, which is when the application closes.`,
+    Beneficiaries will be contacted at most three weeks from Friday 15, which is when the application closes.`,
   PARA3:
     'No need to contact us, you can follow us on our social media pages: @edustipend - Twitter, Instagram and Facebook where we will publish more information.',
   PARA4: `We wish you all the best and as always, we are rooting for you!`,
@@ -12,7 +12,7 @@ export const constants = {
   NEW_APPLICATION_HEADER: "Ready to send it off?",
   NEW_APPLICATION_PARA1: "You've reached the final step of your application journey!, now let's submit your hard work and leave the rest to us.",
   NEW_APPLICATION_PARA2: `Once you hit the submit button, our team will carefully review your application. If you're selected  for the category you applied to, 
-  we'll reach out to you using the email you provided. Beneficiaries will be contacted at most three weeks from Friday 26, which is when the application closes.`,
+  we'll reach out to you using the email you provided. Beneficiaries will be contacted at most three weeks from Friday 15, which is when the application closes.`,
   PROMPT:"Check your email for a verification link as your account is unverified",
 };
 
