a1ce28b9bd7cde806b0a9b1b4422836a5ddecd13
josh-alhassan <saintjosh26@gmail.com>
2024-07-18 23:33:45 +0100

    complete desktop screen

--- src/pages/refer-page/ReferPage.jsx
+++ src/pages/refer-page/ReferPage.jsx
@@ -3,6 +3,7 @@ import styles from './ReferPage.module.css';
 import donationNetworkSVG from '../../assets/refer-frame.svg';
 import Header from '../../components/Header';
 import FormInput from '../../components/FormInput/FormInput';
+import Button from '../../components/Button';
 
 function ReferPage() {
   return (
@@ -22,6 +23,16 @@ function ReferPage() {
           <form action="" className={styles.referForm}>
             <FormInput label="Name" type="text" placeholder="Enter name" />
             <FormInput label="Email" type="email" placeholder="Enter email address" />
+
+            <div className={styles.referFormButton}>
+              <Button size="medium" type="secondary" label="Generate Link" />
+            </div>
+
+            <p className={styles.referralText}>Share your unique referral link</p>
+            <div className={styles.referralLink}>
+              <p>https/edustipend.com/refer/ezebillions</p>
+              <p className={styles.referralLinkCopy}>Copy</p>
+            </div>
           </form>
         </div>
       </Container>
