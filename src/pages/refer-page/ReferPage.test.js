fb470f3c6499b597066445a48470f26f48a5b990
josh-alhassan <saintjosh26@gmail.com>
2024-07-21 12:23:14 +0100

    fix copy feature in test

--- src/pages/refer-page/ReferPage.test.js
+++ src/pages/refer-page/ReferPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import ReferPage from './ReferPage';
 import referPageTexts from './constants';
 
@@ -17,5 +17,27 @@ describe('ReferPage', () => {
 
     expect(screen.getByText(referPageTexts.referralLink)).toBeInTheDocument();
     expect(screen.getByText(referPageTexts.referralLinkCopy)).toBeInTheDocument();
+  });
+
+  test('copies referral link to clipboard', async () => {
+    render(<ReferPage />);
+
+    // Mock the clipboard API
+    const writeTextMock = jest.fn().mockResolvedValue();
+    navigator.clipboard = { writeText: writeTextMock };
+
+    // Simulate clicking the "Copy" button
+    fireEvent.click(screen.getByText(referPageTexts.referralLinkCopy));
+
+    // Check if the referral link was copied to the clipboard
+    expect(writeTextMock).toHaveBeenCalledWith(referPageTexts.referralLink);
+
+    // Check if the "Copied!" message is displayed
+    // eslint-disable-next-line testing-library/prefer-find-by
+    await waitFor(() => expect(screen.getByText('Copied!')).toBeInTheDocument());
+
+    // Check if the "Copied!" message disappears after 2 seconds
+    await new Promise((r) => setTimeout(r, 2000));
+    expect(screen.queryByText('Copied!')).not.toBeInTheDocument();
   });
 });
