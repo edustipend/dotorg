5ab8d202e07f9cfb23e9b649c165ba8050e1f7f9
josh-alhassan <saintjosh26@gmail.com>
2024-08-07 01:36:30 +0100

    resolve atTwo file

--- src/pages/at-two/AtTwo.jsx
+++ src/pages/at-two/AtTwo.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { TestId } from './constants';
 import AtTwoHero from '../../sections/AtTwo/AtTwoHero/AtTwoHero';
+import Leaderboard from '../refer-page/internals';
 import styles from './AtTwo.module.css';
+import BeneficiaryTestimonies from '../../sections/SupportALearner/BeneficiaryTestimonies/BeneficiaryTestimonies';
 
 function AtTwo() {
   return (
     <main data-testid={TestId.AT_TWO} className={styles.main}>
       <AtTwoHero />
+
+      <Leaderboard />
+      <BeneficiaryTestimonies />
     </main>
   );
 }
