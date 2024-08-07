311b6c32433b52b68494bb56af9a2b0af64550d2
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2024-08-07 13:35:26 +0100

    fix: prevent overflowing effect on mobile

--- src/pages/at-two/AtTwo.jsx
+++ src/pages/at-two/AtTwo.jsx
@@ -5,13 +5,16 @@ import HowToWin from '../../sections/AtTwo/HowToWin';
 import Leaderboard from '../refer-page/internals';
 import BeneficiaryTestimonies from '../../sections/SupportALearner/BeneficiaryTestimonies/BeneficiaryTestimonies';
 import styles from './AtTwo.module.css';
+import Container from '../../components/Container';
 
 export const AtTwo = () => {
   return (
     <main data-testid={TestId.AT_TWO} className={styles.main}>
       <AtTwoHero />
       <HowToWin />
-      <Leaderboard />
+      <Container>
+        <Leaderboard />
+      </Container>
       <BeneficiaryTestimonies />
     </main>
   );
