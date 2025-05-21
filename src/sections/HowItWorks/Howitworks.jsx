c4558bf48b067e10da43ebb59c159effffe5896a
Ezeh20 <ranter1570@gmail.com>
2025-05-21 13:42:02 +0100

    chore: removed the accordion component from the how it works page

--- src/sections/HowItWorks/Howitworks.jsx
+++ src/sections/HowItWorks/Howitworks.jsx
@@ -4,16 +4,11 @@ import Container from '../../components/Container';
 import RRRRCard from './internals/RRRRCard';
 import Header from '../../components/Header';
 import { Howitworks, Text } from './constants';
-import Accordion from '../../components/Accordion';
 
 const HowItWorks = () => {
   return (
     <div id="how-it-works" className="how-it-works-container">
       <Container>
-      <Accordion question='Who can apply for the Impact Leaders Program?' answer='Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program?'/>
-      <Accordion question='Who can apply for the Impact Leaders Program?' answer='Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program?'/>
-      <Accordion question='Who can apply for the Impact Leaders Program?' answer='Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program?'/>
-      <Accordion question='Who can apply for the Impact Leaders Program?' answer='Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program? Who can apply for the Impact Leaders Program?'/>
         <Header className="v2-section-header ta-center" color="secondary" size="small">
           {Text.HEADER_TEXT}
         </Header>
