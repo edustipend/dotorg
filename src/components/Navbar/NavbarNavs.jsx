ebabfff47741722ff2a9f0e909fa8aa46b5024a2
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2025-07-23 15:58:20 +0100

    stage: stage current changes

--- src/components/Navbar/NavbarNavs.jsx
+++ src/components/Navbar/NavbarNavs.jsx
@@ -1,41 +1,58 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Button from '../Button';
 import { BUTTON_TYPE, NAVBAR_LINKS, TestId } from './constants';
-import './styles.css';
 import useHandleCTAClick from '../../hooks/useHandleCTAClick';
 import { RenderLinks } from './RenderLinks';
+import { routesConstant } from '../../routesConstant';
+import { content } from '../../sections/Communities/Hero/constants';
+
+import './styles.css';
 
 const { NAVBAR_LINKS_ID } = TestId;
 
 const NavbarNavs = ({ showMenu, closeMenu, path }) => {
   const { buttonLabel, handleCTAClick } = useHandleCTAClick();
+  const isCommunity = path === routesConstant.COMMUNITIES;
 
   return (
     <>
       <nav className="navbarNavs" data-testid={NAVBAR_LINKS_ID}>
         <div className="navContent">
-          <RenderLinks showMenu={showMenu} closeMenu={closeMenu} links={NAVBAR_LINKS} path={path} />
+          <RenderLinks showMenu={showMenu} closeMenu={closeMenu} links={isCommunity ? NAVBAR_LINKS.slice(0, -1) : NAVBAR_LINKS} path={path} />
         </div>
         <div className="navAction">
-          <Button label={buttonLabel} type={BUTTON_TYPE} onClick={() => handleCTAClick()} className="navBtn" />
+          {isCommunity ? (
+            <Link to={content.linkUrl} target={content.linkTarget}>
+              <Button type={BUTTON_TYPE} label={content.btnLabel} className="navBtn" />
+            </Link>
+          ) : (
+            <Button label={buttonLabel} type={BUTTON_TYPE} onClick={() => handleCTAClick()} className="navBtn" />
+          )}
         </div>
       </nav>
 
       {showMenu ? (
         <nav className="mobile-nav">
           <div className="mobileNavContent">
-            <RenderLinks showMenu={showMenu} closeMenu={closeMenu} links={NAVBAR_LINKS} path={path} />
+            <RenderLinks showMenu={showMenu} closeMenu={closeMenu} links={isCommunity ? NAVBAR_LINKS.slice(0, -1) : NAVBAR_LINKS} path={path} />
           </div>
           <div className="mobile-nav-btn">
-            <Button
-              label={buttonLabel}
-              type={BUTTON_TYPE}
-              onClick={() => {
-                closeMenu(!showMenu);
-                handleCTAClick();
-              }}
-              className="navBtn"
-            />
+            {isCommunity ? (
+              <Link to={content.linkUrl} target={content.linkTarget}>
+                <Button type={BUTTON_TYPE} label={content.btnLabel} className="navBtn" />
+              </Link>
+            ) : (
+              <Button
+                label={buttonLabel}
+                type={BUTTON_TYPE}
+                onClick={() => {
+                  closeMenu(!showMenu);
+                  handleCTAClick();
+                }}
+                className="navBtn"
+              />
+            )}
           </div>
         </nav>
       ) : (
--- src/sections/SupportALearner/Partners/Partners.jsx
+++ src/sections/SupportALearner/Partners/Partners.jsx
@@ -3,17 +3,22 @@ import Container from '../../../components/Container';
 import styles from '../Partners/Partners.module.css';
 import Header from '../../../components/Header';
 import { TestId, Texts } from '../Partners/contants';
+import { useLocation } from 'react-router-dom';
+import { routesConstant } from '../../../routesConstant';
 
 const images = [...Array(6)].map((_, index) => require(`../../../assets/partner${index + 1}.svg`));
 
 const Partners = () => {
+  const { pathname } = useLocation();
+  const isCommunities = pathname === routesConstant.COMMUNITIES;
+
   return (
     <div className={styles.container} data-testid={TestId.WRAPPER}>
       <Container>
         <div className={styles.headerContainer}>
           <div className={styles.headerWrap}>
             <Header size={'large'} dataTest={TestId.HEAD_TEXT}>
-              {Texts.HEADER}
+              {isCommunities ? Texts.COMMUNITIES_HEADER : Texts.HEADER}
             </Header>
             <p data-testid={TestId.DESCRIPTION}>
               {Texts.COLLABORATE}{' '}
